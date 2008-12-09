98fb762131a061a515929e53aaba2e07ccf583b5
Patrice Neff <mail@patrice.ch>
2008-12-09 11:34:43 +0100

    Fix for SQLAlchemy 0.5

--- scrumboard/model/__init__.py
+++ scrumboard/model/__init__.py
@@ -8,7 +8,7 @@ from scrumboard.model import meta
 
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
-    sm = orm.sessionmaker(autoflush=True, transactional=True, bind=engine)
+    sm = orm.sessionmaker(autoflush=True, autocommit=False, bind=engine)
     meta.engine = engine
     meta.Session = orm.scoped_session(sm)
 
