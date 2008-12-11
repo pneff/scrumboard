6fb6293d3926d7f2958013fb8eec3428eef9d48f
Patrice Neff <mail@patrice.ch>
2008-12-11 14:55:25 +0100

    Open story list with a link.

--- scrumboard/public/static/sprints/selector.js
+++ scrumboard/public/static/sprints/selector.js
@@ -13,7 +13,23 @@
         });
     };
     
-    YAHOO.util.Event.onContentReady('stories', function() {
-        storyTable = new YAHOO.scrumboard.stories.scrollTable('stories');
+    YAHOO.util.Event.onContentReady('stories_link', function() {
+        YAHOO.util.Event.on('stories_link', 'click', function(ev) {
+            YAHOO.util.Event.preventDefault(ev);
+            if (storyTable === null) {
+                storyTable = new YAHOO.scrumboard.stories.scrollTable('stories');
+            }
+            YAHOO.util.Dom.removeClass('stories', 'hidden');
+            YAHOO.util.Dom.removeClass('stories_close_link', 'hidden');
+            YAHOO.util.Dom.addClass('stories_link', 'hidden');
+        });
+    });
+    YAHOO.util.Event.onContentReady('stories_close_link', function() {
+        YAHOO.util.Event.on('stories_close_link', 'click', function(ev) {
+            YAHOO.util.Event.preventDefault(ev);
+            YAHOO.util.Dom.addClass('stories', 'hidden');
+            YAHOO.util.Dom.addClass('stories_close_link', 'hidden');
+            YAHOO.util.Dom.removeClass('stories_link', 'hidden');
+        });
     });
 })();
